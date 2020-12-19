d187fe6b2c5e2b09877a26552be046687c14f6ee
Gunish Matta <gunishmatta@gmail.com>
2020-12-19 09:29:40 +0530

    AJAX Programs

---
+++ demo_get2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		GET Method PHP File
+	</title>
+</head>
+<body>
+
+Hello <?php 
+echo $_GET["fname"] ;?> <br>
+
+Mr. <?php echo $_GET["lname"] ?>
+
+
+</body>
+</html>
